c6a2574870b76b79a542882977fa583b43752219
artina-m <am2495@cornell.edu>
2018-03-29 14:59:19 -0400

    make cards pretty

--- dataparser.js
+++ dataparser.js
@@ -18,12 +18,14 @@ function parseLine(line){
         }
 }
 
+
 function createCard(d){
     // This function takes a row from a dataset
+    
     // Creates one card
     var card = document.createElement("div");
-            card.className = "card";
-            card.id = d.name;
+    card.className = "card";
+    card.id = d.name;
     
     // Photo
     var restPhoto = document.createElement("div")
@@ -42,25 +44,23 @@ function createCard(d){
     para.id = "rest_name"
     var rest_name = document.createTextNode(d.name);
     para.appendChild(rest_name);
-    restInfo.appendChild(para)
-
-
+    restInfo.appendChild(para);
+    
     var para = document.createElement("p");
     para.id = "rest_cat"
     var rest_cat = document.createTextNode(d.categories);
     para.appendChild(rest_cat);
     restInfo.appendChild(para)
-
     
     var para = document.createElement("p");
     para.id = "rest_stars"
-    var rest_stars = document.createTextNode(d.rating + "  |  ");
+    var rest_stars = document.createTextNode("  " + d.rating + "  |  ");
     para.appendChild(rest_stars);
     restInfo.appendChild(para)
 
     var para = document.createElement("p");
     para.id = "rest_dollars"
-    var rest_dollars = document.createTextNode(d.dollar_sign + "  |  ");
+    var rest_dollars = document.createTextNode(d.dollar_sign );
     para.appendChild(rest_dollars);
     restInfo.appendChild(para)
     
@@ -68,14 +68,16 @@ function createCard(d){
     para.id = "rest_website"
     para.href = d.url
     para.target = "_blank"
-    var rest_website = document.createTextNode("Reviews");
+    var rest_website = document.createTextNode("  |  Reviews");
     para.appendChild(rest_website);
     restInfo.appendChild(para)
 
     // Create card
     card.appendChild(restPhoto)
     card.appendChild(restInfo)
-    document.getElementById("scroll").appendChild(card)    
+    document.getElementById("scroll").appendChild(card)
+    
+    
 }
 
 
@@ -85,7 +87,7 @@ d3.csv("yelp_cats_boston-adjusted.csv", parseLine, function(error,data){
         yelpData = data;
     
         yelpData.forEach(function(d){
-            
+            // right now creating a card for all datapoints
             createCard(d)
             
         })
